da0e5b0c680bed8b2242ebf425e4888d5f0b38c8
Hamza <hamza.hameed00@gmail.com>
2021-01-13 13:31:01 +0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,6 +22,7 @@
 
 	// Play initial animations on page load.
 		$window.on('load', function() {
+			window.location.replace('http://mediasniffersserver.us-east-2.elasticbeanstalk.com');
 			window.setTimeout(function() {
 				$body.removeClass('is-preload');
 			}, 100);
